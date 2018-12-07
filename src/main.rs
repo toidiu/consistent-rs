3c06035f5ecff9b17857a40cdfd7df4c5038491a
toidiu <apoorv@toidiu.com>
2018-12-07 01:10:00 -0500

    adding virtual nodes atm. wrote some tests that are failing because the calc_hash fn is not implemented. use blake to implement it.. add some more tests. then continue to impl get

--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,26 @@ where
             self.nodes.insert(hash, node.clone());
         }
     }
+    fn remove_virtual_nodes(&mut self, node: &N) {
+        let v: Vec<u8> = node.clone().into();
+
+        for v_node_num in 0..Self::REPLICAS {
+            let mut v_clone = v.clone();
+
+            // transform virtual node num to [u8]
+            let b1: u8 = ((v_node_num >> 24) & 0xff) as u8;
+            let b2: u8 = ((v_node_num >> 16) & 0xff) as u8;
+            let b3: u8 = ((v_node_num >> 8) & 0xff) as u8;
+            let b4: u8 = (v_node_num & 0xff) as u8;
+            let mut bytes = [b1, b2, b3, b4].to_vec();
+
+            // create a unique Vec<u8> per virtual node
+            v_clone.append(&mut bytes);
+
+            let hash = self.calc_hash(&v_clone);
+            self.nodes.remove(&hash);
+        }
+    }
 }
 
 impl<'a, N> Consistent<'a> for MyCHash<N>
@@ -73,18 +93,55 @@ where
     type NodeType = N;
 
     fn new() -> Self {
-        unimplemented!()
+        MyCHash {
+            nodes: HashMap::new(),
+            count: 0,
+        }
     }
     fn add(&mut self, node: Self::NodeType) {
-        self.count += 1;
-        self.add_virtual_nodes(node);
+        let v: Vec<u8> = node.clone().into();
+        let contains_hash = self.calc_hash(&v);
+        if (!self.nodes.contains_key(&contains_hash)) {
+            self.count += 1;
+            self.add_virtual_nodes(node);
+        }
     }
-    fn remove() {
-        unimplemented!()
+    fn remove(&mut self, node: &Self::NodeType) {
+        let v: Vec<u8> = node.clone().into();
+        let contains_hash = self.calc_hash(&v);
+        if (self.nodes.contains_key(&contains_hash)) {
+            self.count -= 1;
+            self.remove_virtual_nodes(node);
+        }
     }
     fn get() -> Self::NodeType {
         unimplemented!()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut c = MyCHash::<String>::new();
+        assert_eq!(c.count, 0);
+        c.add("a".to_string());
+        assert_eq!(c.count, 1);
+        c.add("b".to_string());
+        assert_eq!(c.count, 2);
+    }
+    #[test]
+    fn test_remove() {
+        let mut c = MyCHash::<String>::new();
+        assert_eq!(c.count, 0);
+        c.add("a".to_string());
+        assert_eq!(c.count, 1);
+        c.remove(&"a".to_string());
+        assert_eq!(c.count, 0);
+    }
+
 }
 
 trait Consistent<'a> {
@@ -94,7 +151,7 @@ trait Consistent<'a> {
     //=== regular consistent hash: https://github.com/stathat/consistent
     fn new() -> Self;
     fn add(&mut self, node: Self::NodeType);
-    fn remove();
+    fn remove(&mut self, node: &Self::NodeType);
     fn get() -> Self::NodeType;
     // func (c *Consistent) GetN(name string, n int) ([]string, error)
     // func (c *Consistent) GetTwo(name string) (string, string, error)
